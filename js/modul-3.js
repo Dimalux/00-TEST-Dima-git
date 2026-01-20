69840d7e9ae5145d82ac9afaa6e3111536490c52
Dima_V <astrallux777@ukr.net>
2026-01-20 23:04:42 +0200

    Modul-3-5

--- js/modul-3.js
+++ js/modul-3.js
@@ -81,6 +81,84 @@
 // ............................
 
 
+// ЗАДАЧА 4
+// Напиши скрипт, який виводить у консоль ім'я та телефонний номер користувача
 
+// Варіант МІЙ
+// const names = "Jacob,William,Solomon,Artemis";
+// const phones = "380012345671,380011122334,380055566377,380055566300";
+
+// const arr1 = names.split(",");
+// const arr2 = phones.split(",");
+
+// console.log(arr1);
+// console.log(arr2);
+
+// const arr3 = arr1.concat(arr2);
+// console.log(arr3);
+
+// for (let i = 0; i < (arr3.length / 2); i++) {
+//    console.log(`${arr3[i]}: ${arr3[i + (arr3.length / 2)]}`);      
+// }
+
+
+
+// Варіант МЕНТОР
+// const names = "Jacob,William,Solomon,Artemis";
+// const phones = "380012345671,380011122334,380055566377,380055566300";
+
+// const namesArr = names.split(",");
+// const phonesArr = phones.split(",");
+
+// console.log(namesArr);
+// console.log(phonesArr);
+
+
+// for (let i = 0; i < namesArr.length; i++) {
+//    console.log(`${namesArr[i]}: ${phonesArr[i]}`);      
+// }
+
+
+// ....................................
+
+
+// ЗАДАЧА 5
+
+// Напиши скрипт, який виводить у консоль усі слова рядка
+// крім першого і останнього. Результуючий рядок не повинен починатися
+// або закінчуватися символ пробілу.
+// Скрипт повинен працювати для будь-якого рядка.
+
+
+// Варіант МІЙ
+// const string = "   Welcome to the future    ";
+
+// const arr = string.trim().split(" ");
+// console.log(arr);
+
+// let arrRes = [];
+// for (let i = 0; i < (arr.length - 2); i++) {
+    
+//     arrRes.push(arr[i + 1]);     
+// }
+
+// console.log(arrRes);
+// const result = arrRes.join(" ");
+// console.log(result);
+
+
+
+
+// Варіант МЕНТОР
+// const string = "   Welcome     to the       future    ";
+// const arr = string.trim().split(" ");
+// console.log(arr);
+
+// const arrRes = arr.slice(1, arr.length -1).join(" ").trim();
+
+// console.log(arrRes);
+
+
+// .............................
 
 
