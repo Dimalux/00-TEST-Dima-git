8941f7cb327429abbbac29dca12305ef3c35149b
Dima_V <astrallux777@ukr.net>
2026-01-25 23:16:41 +0200

    Modul-4

---
+++ js/modul-4.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+// ЗАДАЧА 1
+// Напиши скрипт для обчислення площі прямокутника зі сторонами, значення яких зберігаються у змінній values у вигляді рядка.
+// Значення гарантовано розділені пробілом.
+
+// const value = "10 10";
+
+// const arrs = value.split(" ");
+// console.log(arrs);
+
+// let arrNew = [];
+
+// for (const arr of arrs) {
+//     arrNew.push(Number(arr));        
+//     }
+//     console.log(arrNew);
+
+//     console.log(arrNew[0] * arrNew[1]);
+    
+    
