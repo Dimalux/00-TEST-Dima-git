450cf150993070c83bbd9e702d4640890a7b6007
Dima_V <astrallux777@ukr.net>
2026-01-27 16:00:15 +0200

    Modul-4-2

--- js/modul-4.js
+++ js/modul-4.js
@@ -9,26 +9,38 @@
 // - замінює значення premium на false
 // - виводить вміст об'єкта user у форматі ключ:значення використовуючі Object.keys() та for...of
 
-const user = {
-    name: "Alice",
-    age: 25,
-    hobby: "html",
-    premium: true,
-};
+// const user = {
+//     name: "Alice",
+//     age: 25,
+//     hobby: "html",
+//     premium: true,
+// };
 
-user.mood = "happy";
-user.hobby = "skydiving";
-user.premium = false;
+// user.mood = "happy";
+// user.hobby = "skydiving";
+// user.premium = false;
 
-const userNew = Object.keys(user);
+// const userNew = Object.keys(user);
 
-for(const item of userNew) {
-console.log(`${item}: ${user[item]}`);
-}
+// for(const item of userNew) {
+// console.log(`${item}: ${user[item]}`);
+// }
 
 
 // .............................
 
+// ЗАДАЧА 1 Напиші функцію, щоб перевіряла си присутній такий товар в масиві storage
+
+
+
+function foo(storage, item) {
+   
+if(storage.includes(item.toLowerCase()))
+
+
+}
+
+console.log(["apple", "plum", "pear"], "pEar");
 
 
 
