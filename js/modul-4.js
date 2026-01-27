47c8263db15c746612411ab6f4c1d58348d16275
Dima_V <astrallux777@ukr.net>
2026-01-27 21:56:46 +0200

    Modul-4-2

--- js/modul-4.js
+++ js/modul-4.js
@@ -29,18 +29,57 @@
 
 // .............................
 
-// ЗАДАЧА 1 Напиші функцію, щоб перевіряла си присутній такий товар в масиві storage
+// ЗАДАЧА 2   Напиші функцію, щоб перевіряла си присутній такий товар в масиві storage
 
+// Варіант-1 - відповідь (суворе порівняння)
 
-
-// function foo(storage, item) {
-   
-// if(storage.includes(item.toLowerCase()))
-
-
+// function foo(storage, item) {   
+// if(storage.includes(item.toLowerCase())){
+//     return `OK: ${item.toLowerCase()}`
+// };
+// return "Sorry, nothing found!"
 // }
 
-// console.log(["apple", "plum", "pear"], "pEar");
+// console.log(foo(["apple", "plum", "pear"], "plum"));
 
 
+// Варіант-2 - якщо користувач написав неповне слово
+
+// function foo(storage, item) {   
+
+// const itemTest = item.toLowerCase();
+
+// Приводимо масив до рядка
+// const storageNew = storage.join(" ");
+
+// Застосовуємо метод  indexOf()  до рядка (перевірка першого входження підрядка (НЕПОВНОГО СЛОВА) в рядок). 
+// Результат:
+// - індекс першого входження (індекс першого символу) підрядка, якщо він знайдений;
+//  або
+// - "-1", якщо підрядок не виявлено.
+
+// if(storageNew.indexOf(itemTest) !== -1){
+//     return `OK: ${itemTest}`
+// };
+
+// Або 
+// Метод includes() для перевірки ля перевірки наявності підрядка (НЕПОВНОГО СЛОВА) у рядку. 
+// Він повертає логічне значення true, якщо підрядок знайдено, і false, якщо підрядок відсутній.
+
+
+// for(const itemArr of storage) {
+// if(itemArr.includes(itemTest)) {
+//     return `OK: ${itemArr}`
+// };
+// }
+
+// return "Sorry, nothing found!"
+// }
+
+// console.log(foo(["apple", "plum", "pear"], "pe"));
+
+
+// .............................
+
+// ЗАДАЧА 3
 
