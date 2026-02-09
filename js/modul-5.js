3af1c664609ce936f050c3bae4ec8f67366aad01
Dima_V <astrallux777@ukr.net>
2026-02-09 15:52:40 +0200

    Modul-5-2-2

--- js/modul-5.js
+++ js/modul-5.js
@@ -115,31 +115,32 @@
 // ПОЯСНЕННЯ-2  Інлайн-колбеки
 // Якщо колбек-функція маленька і потрібна тільки для передачі аргументом, її можна оголосити безпосередньо на момент виклику функції, в яку передаємо колбек. Такі функції називаються інлайн-колбеки. Вони будуть доступні тільки в якості значення параметра і більше ніде в коді.
 
-// function registerGuest(name, callback) {
-//   console.log(`Registering ${name}!`);
-//   callback(name);
-// }
+function registerGuest(name, callback) {
+  console.log(`\nRegistering ${name}!`);
+  callback(name);
+}
 
-// // 1)  Викликаємо функцію registerGuest() :
+// 1)  Викликаємо функцію registerGuest() :
 
-// // Передаємо інлайн-функцію "greet" у якості колбека
-// registerGuest("Mango", function greet(name) {
-//   console.log(`Welcome ${name}!`);
-// });
+// Передаємо інлайн-функцію "greet" у якості колбека
+registerGuest("Mango", function greet(name) {
+  console.log(`Welcome ${name}!`);
+});
 
-// console.log("");
+
 
 // //...................
 
 // // 2)  Викликаємо функцію registerGuest() :
 
-// // Передаємо інлайн-функцію "notify" у якості колбека
-// registerGuest("Poly", function notify(name) {
-//   console.log(`Dear ${name}, your room will be ready in 30 minutes`);
-// });
+// Передаємо інлайн-функцію "notify" у якості колбека
+registerGuest("Poly", function notify(name) {
+  console.log(`Dear ${name}, your room will be ready in 30 minutes`);
+});
 
 // .............................
 // .............................
+
 
 
 
