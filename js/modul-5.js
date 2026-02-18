bf1165ae9082a64393aa3c13f86bebba1a310ea1
Dima_V <astrallux777@ukr.net>
2026-02-18 23:21:17 +0200

    Modul-5-U-5

--- js/modul-5.js
+++ js/modul-5.js
@@ -1218,3 +1218,66 @@
 
 
 // ЗАДАЧА U-5
+// Нехай функція getModels повертає масив моделей (поле "model") всіх автомобілей
+
+
+// Варіант 1  метод  "forEach" :
+
+// const allCars = [
+//     { make: "Honda", model: "CR-V", amount: 14, price: 24045 },
+//     { make: "Honda", model: "Accord", amount: 2, price: 22455 },
+//     { make: "Mazda", model: "Mazda 6", amount: 8, price: 24195 },
+//     { make: "Mazda", model: "CX-9", amount: 7, price: 31520 },
+//     { make: "Toyota", model: "4Runner", amount: 19, price: 34210 },
+//     { make: "Toyota", model: "Sequoia", amount: 16, price: 45560 },
+//     { make: "Toyota", model: "Tacoma", amount: 4, price: 24320 },
+//     { make: "Ford", model: "F-150", amount: 11, price: 27110 },
+//     { make: "Ford", model: "Fusion", amount: 13, price: 22120 },
+//     { make: "Ford", model: "Explorer", amount: 6, price: 31660 }
+// ];
+
+// const getModels = (arr) => {
+// const sumModel =[];
+
+// arr.forEach(item => {
+//     sumModel.push(item.model)
+// });
+
+// return sumModel;
+// }
+
+// console.log(getModels(allCars));
+
+
+// ............
+
+
+// Варіант 2  метод  "map" :
+
+// const allCars = [
+//     { make: "Honda", model: "CR-V", amount: 14, price: 24045 },
+//     { make: "Honda", model: "Accord", amount: 2, price: 22455 },
+//     { make: "Mazda", model: "Mazda 6", amount: 8, price: 24195 },
+//     { make: "Mazda", model: "CX-9", amount: 7, price: 31520 },
+//     { make: "Toyota", model: "4Runner", amount: 19, price: 34210 },
+//     { make: "Toyota", model: "Sequoia", amount: 16, price: 45560 },
+//     { make: "Toyota", model: "Tacoma", amount: 4, price: 24320 },
+//     { make: "Ford", model: "F-150", amount: 11, price: 27110 },
+//     { make: "Ford", model: "Fusion", amount: 13, price: 22120 },
+//     { make: "Ford", model: "Explorer", amount: 6, price: 31660 }
+// ];
+
+// Запис із фігурними дужками (ЯВНЕ поверненя explicit return) :
+// const getModels = (arr) => {return arr.map(item => item.model);}
+
+// або в стрілках використовуємо запис без фігурних дужок (НЕЯВНЕ поверненя implicit return) :
+// const getModels = arr => arr.map(item => item.model);
+
+// console.log(getModels(allCars));
+
+
+// .............................
+// .............................
+
+
+// ЗАДАЧА U-6
