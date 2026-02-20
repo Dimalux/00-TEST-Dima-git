3601c95f5bd3badbea8f48ddb92bea1d86d8ef4f
Dima_V <astrallux777@ukr.net>
2026-02-20 19:56:34 +0200

    Modul-5-U-6-3

--- js/modul-5.js
+++ js/modul-5.js
@@ -1343,6 +1343,31 @@
 // console.log(allCars);
 
 
+// ...............
+
+
+// Варіант 3  (Ментор урок на 2:00:00) :
+// const allCars = [
+//     { make: "Honda", model: "CR-V", amount: 14, price: 24045 },
+//     { make: "Honda", model: "Accord", amount: 2, price: 22455 },
+//     { make: "Mazda", model: "Mazda 6", amount: 8, price: 24195 },
+//     { make: "Mazda", model: "CX-9", amount: 7, price: 31520 },
+//     { make: "Toyota", model: "4Runner", amount: 19, price: 34210 },
+//     { make: "Toyota", model: "Sequoia", amount: 16, price: 45560 },
+//     { make: "Toyota", model: "Tacoma", amount: 4, price: 24320 },
+//     { make: "Ford", model: "F-150", amount: 11, price: 27110 },
+//     { make: "Ford", model: "Fusion", amount: 13, price: 22120 },
+//     { make: "Ford", model: "Explorer", amount: 6, price: 31660 }
+// ];
+
+// const makeCarsWithDiscount = (cars, discount) => 
+// {return cars.map(car => ({...car, price: car.price * (1 - discount)}));  
+// };
+
+// console.log(makeCarsWithDiscount(allCars, 0.2));
+// console.log(allCars);
+
+
 // .............................
 // .............................
 
