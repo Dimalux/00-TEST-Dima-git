acd39c50d2a247b8618e798d687269569a99ea74
Dima_V <astrallux777@ukr.net>
2026-02-21 22:37:08 +0200

    Modul-5-U-10

--- js/modul-5.js
+++ js/modul-5.js
@@ -1478,4 +1478,24 @@
 // .............................
 
 
-// ЗАДАЧА U-10
+// ЗАДАЧА U-10  Метод filter(callback)
+// Нехай функція "filterByMake" повертає масив автомобілів за маркою :
+
+// const cars = [
+//   { make: "Honda", model: "CR-V", type: "suv", price: 24045 },
+//   { make: "Honda", model: "Accord", type: "sedan", price: 22455 },
+//   { make: "Mazda", model: "Mazda 6", type: "sedan", price: 24195 },
+//   { make: "Mazda", model: "CX-9", type: "suv", price: 31520 },
+//   { make: "Toyota", model: "4Runner", type: "suv", price: 34210 },
+//   { make: "Toyota", model: "Sequoia", type: "suv", price: 45560 },
+//   { make: "Toyota", model: "Tacoma", type: "truck", price: 24320 },
+//   { make: "Ford", model: "F-150", type: "truck", price: 27110 },
+//   { make: "Ford", model: "Fusion", type: "sedan", price: 22120 },
+//   { make: "Ford", model: "Explorer", type: "suv", price: 31660 }
+// ];
+
+// const filterByMake = (arr, getMake) => arr.filter(item => item.make === getMake);
+
+//  console.log(filterByMake(cars, "Ford"));
+
+ 
