321e76043768cf4e2985af64bf0286d2a3e0cb8a
Dima_V <astrallux777@ukr.net>
2026-03-17 23:18:16 +0200

    Modul-6-26

--- js/modul-6.js
+++ js/modul-6.js
@@ -1162,4 +1162,69 @@
 
 // ПОЯСНЕННЯ-26
 
+// Напишіть метод calcTotalPrice(stoneName), який приймає назву каменю і
+// розраховує та повертає загальну вартість каменів з таким ім'ям, ціною та
+// кількістю з властивості stones.
 
+// Варіант (МІЙ) :
+
+// const chopShop = {
+//     stones: [
+//     { name: "Emerald", price: 1300, quantity: 4 },
+//     { name: "Diamond", price: 2700, quantity: 3 },
+//     { name: "Sapphire", price: 1400, quantity: 7 },
+//     { name: "Ruby", price: 800, quantity: 2 },
+//     ],
+
+// calcTotalPrice(stoneName) {
+//     let result = 0;
+
+// this.stones.map(stone => {
+//     if(stoneName === stone.name) {
+// result = stone.price * stone.quantity;
+// }
+// }
+// )
+// return result
+// }
+
+// };
+
+// console.log(chopShop.calcTotalPrice("Ruby"));      // 1600
+// console.log(chopShop.calcTotalPrice("Emerald"));   // 5200
+// console.log(chopShop.calcTotalPrice("Sapphire"));  // 9800
+// console.log(chopShop.calcTotalPrice("Diamond"));   // 8100
+
+
+// .............
+
+
+// Варіант (чат GPT) :
+
+// const chopShop = {
+//     stones: [
+//     { name: "Emerald", price: 1300, quantity: 4 },
+//     { name: "Diamond", price: 2700, quantity: 3 },
+//     { name: "Sapphire", price: 1400, quantity: 7 },
+//     { name: "Ruby", price: 800, quantity: 2 },
+//     ],
+
+// calcTotalPrice(stoneName) {
+//     const stone = this.stones.find(stone => stone.name === stoneName);
+//     return stone.price * stone.quantity;
+// }
+
+// };
+
+// console.log(chopShop.calcTotalPrice("Ruby"));      // 1600
+// console.log(chopShop.calcTotalPrice("Emerald"));   // 5200
+// console.log(chopShop.calcTotalPrice("Sapphire"));  // 9800
+// console.log(chopShop.calcTotalPrice("Diamond"));   // 8100
+
+
+
+// .............................
+// .............................
+
+
+// ПОЯСНЕННЯ-27
