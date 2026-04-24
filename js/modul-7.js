5727323c5da4ad868768be89363c40192441c7f9
Dima_V <astrallux777@ukr.net>
2026-04-24 22:51:09 +0300

    M7-DZ-2-3

--- js/modul-7.js
+++ js/modul-7.js
@@ -3258,32 +3258,32 @@
 //     Додай мінімальне оформлення галереї флексбоксами через CSS класи.
 
 
-const images = [
-  {
-    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
-    alt: "White and Black Long Fur Cat",
-  },
-  {
-    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
-    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
-  },
-  {
-    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
-    alt: "Group of Horses Running",
-  },
-  {
-    url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
-    alt: "Alpine Spring Meadows",
-  },
-  {
-    url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
-    alt: "Nature Landscape",
-  },
-  {
-    url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
-    alt: "Lighthouse Coast Sea",
-  },
-];
+// const images = [
+//   {
+//     url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+//     alt: "White and Black Long Fur Cat",
+//   },
+//   {
+//     url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+//     alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+//   },
+//   {
+//     url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+//     alt: "Group of Horses Running",
+//   },
+//   {
+//     url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+//     alt: "Alpine Spring Meadows",
+//   },
+//   {
+//     url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+//     alt: "Nature Landscape",
+//   },
+//   {
+//     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+//     alt: "Lighthouse Coast Sea",
+//   },
+// ];
 
 
 // Варіант-1   Створюємо й додаємо HTML-елементи, використовуючи 
@@ -3338,4 +3338,22 @@ const images = [
 //..............................
 
 
-// ..............
+// Варіант-2   Створюємо й додаємо HTML-елементи, 
+// використовуючи ШАБЛОННІ рядки і "elem.insertAdjacentHTML()" :
+
+// // <ul class="gallery"></ul>
+// const gallery = document.querySelector(".gallery");
+
+// gallery.insertAdjacentHTML("afterbegin", images.map(image => 
+// `<li>
+// <img class="gallery-image" src="${image.url}" alt="${image.alt}" width="300" />
+// </li>
+// `).join(""));
+
+      
+// .............................
+// .............................
+
+
+// ЗАДАЧА DZ-3
+// Задача 3.  Напиши скрипт 
