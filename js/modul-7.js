12d9d138b158018f02069da583a679f67836c6ae
Dima_V <astrallux777@ukr.net>
2026-04-24 13:51:12 +0300

    M7-DZ-2

--- js/modul-7.js
+++ js/modul-7.js
@@ -3240,3 +3240,75 @@
 
 // ЗАДАЧА DZ-2
 // Задача 2.  Напиши скрипт для створення галереї зображень на основі масиву даних. HTML містить список ul.gallery.
+
+
+// <ul class="gallery"></ul>
+
+
+// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>.
+
+// Можна створити й додати HTML-елементи, використовуючи :
+// 1) document.createElement() і elem.append();
+// 2) або шаблонні рядки і elem.insertAdjacentHTML().
+
+//     Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+//     Додай мінімальне оформлення галереї флексбоксами через CSS класи.
+
+
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    alt: "Alpine Spring Meadows",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    alt: "Nature Landscape",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    alt: "Lighthouse Coast Sea",
+  },
+];
+
+
+// Варіант-1   Створюємо й додаємо HTML-елементи, використовуючи 
+// 1) document.createElement() і elem.append() :
+
+// <ul class="gallery"></ul>
+const gallery = document.querySelector(".gallery");
+
+
+images.map(image => {
+
+  const listGallery = document.createElement("li");
+  
+
+  // images.forEach(image => {
+  //   const li = document.createElement("li");
+  //   li.classList.add("gallery-item");
+    
+  //   const img = document.createElement("img");
+  //   img.classList.add("gallery-image");
+  //   img.src = image.url;
+  //   img.alt = image.alt;
+    
+  //   li.appendChild(img);
+  //   fragment.appendChild(li);
+  // });
+
+
+})
+
+console.log(gallery);
