04a036dbfe0c301e7f6e442e9659571e30d81813
Dima_V <astrallux777@ukr.net>
2026-04-25 23:06:40 +0300

    M7-DZ-3

--- js/modul-7.js
+++ js/modul-7.js
@@ -3356,4 +3356,22 @@
 
 
 // ЗАДАЧА DZ-3
-// Задача 3.  Напиши скрипт 
+// Задача 3.  Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input) підставляє його поточне значення в span#name-output як ім'я для привітання. Обов'язково очищай значення в інпуті по краях від пробілів . Якщо інпут порожній або містить лише пробіли, то замість імені у спан має підставлятися рядок "Anonymous".
+
+
+// <input type="text" id="name-input" placeholder="Please enter your name" />
+// <h1>Hello, <span id="name-output">Anonymous</span>!</h1> 
+
+const nameInput = document.querySelector("#name-input");
+const textNameUser = document.querySelector("#name-output");
+
+console.log(nameInput);
+console.log(textNameUser);
+
+// (дивись рядок 1378)
+nameInput.addEventListener("input", handler);
+
+function handler(event) {
+textNameUser.textContent = event.target.value;
+}
+
